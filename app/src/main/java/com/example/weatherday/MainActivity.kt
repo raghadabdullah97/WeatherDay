1e16187e91c4c6eb6808cc01d5e6246c955b870d
Raghad Abdullah <Raghad.Abdullah.ruh.k@tuwaiq.edu.sa>
2022-03-06 13:11:36 +0300

    ADD API KYE

--- app/src/main/java/com/example/weatherday/MainActivity.kt
+++ app/src/main/java/com/example/weatherday/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.weatherday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    // api key from Weather web :
+    val apiKey = "59e91bf17a6a13fba9d2b609b71cf7cb"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.searchButton )
+        button.setOnClickListener{
+            getWeather()
+        }
     }
+    private fun getWeather(){
+        val searchInput = findViewById<EditText>(R.id.searchInput)
+        //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=59e91bf17a6a13fba9d2b609b71cf7cb
+        //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=59e91bf17a6a13fba9d2b609b71cf7cb
+        try {
+        if (searchInput.text.isNotEmpty()) {
+            //URL From Website:
+            var fullURL =
+                "https://home.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=59e91bf17a6a13fba9d2b609b71cf7cb"
+            val queue = Volley.newRequestQueue(this)
+            val stringRequest = StringRequest(Request.Method.POST, fullURL, { response ->
+                println(response.toString())
+
+            }, {}
+
+            )
+        }
+
+    }catch (err:Exception){
+
+
+        }    }
 }
